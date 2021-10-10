c0cf72d56b6e49f1569c860b666cd3e777df27d3
Aaron Liftig <49658070+AaronL87@users.noreply.github.com>
2021-10-10 18:56:02 -0400

    Update algo.py

--- algo.py
+++ algo.py
@@ -1,8 +1,9 @@
 class ADALINE:
-    def __init__(self, A, B, weight, beta, desired_outputs = (-1, 1)):
+    def __init__(self, A, B, weight, beta, pattern_rnd_int, weight_rnd_int,
+                 desired_outputs = (-1, 1), max_iter=50):
         restart = True
         weight_count = 0
-        while restart:
+        while restart and weight_count <= max_iter - 1:
             count = 0
             print(f'Current weight (w{weight_count}): {weight}')
             print()
@@ -10,20 +11,25 @@ class ADALINE:
                 count += 1
                 error_E = self.get_error(a, weight, desired_outputs[1])
                 if error_E != 0:
-                    weight = self.get_new_weight(a, weight, beta, error_E)
+                    weight = self.get_new_weight(a, weight, beta, error_E, 
+                                                pattern_rnd_int, weight_rnd_int)
                     weight_count += 1
                     print(f'after A{count}')
                     break
 
                 error_E = self.get_error(b, weight, desired_outputs[0])
                 if error_E != 0:
-                    weight = self.get_new_weight(b, weight, beta, error_E)
+                    weight = self.get_new_weight(b, weight, beta, error_E, 
+                                                pattern_rnd_int, weight_rnd_int)
                     weight_count += 1
                     print(f'after B{count}')
                     break
             else:
                 restart = False
                 print('finished')
+                exit(1)
+        print('Reached max_iter')
+        exit(0)
 
     def get_error(self, pattern, weight, desired_output):
         net_input_I = self.dot_product(pattern, weight)
@@ -33,10 +39,14 @@ class ADALINE:
             output = -1
         return desired_output - output
 
-    def get_new_weight(self, pattern, weight, beta, error):
-        L = self.get_distance(pattern)
-        delta = beta * error * pattern[0] / L, beta * error * pattern[1] / L
-        return weight[0] + delta[0], weight[1] + delta[1]
+    def get_new_weight(self, pattern, weight, beta, error, pattern_rnd_int, weight_rnd_int):
+        L = self.get_distance(pattern, pattern_rnd_int)
+        delta = (round(beta * error * pattern[0] / L, weight_rnd_int),
+                round(beta * error * pattern[1] / L, weight_rnd_int))
+        print(f'beta: {beta}, E: {error}, x_0: {pattern[0]}, x_1: {pattern[1]}, L: {L}')
+        print(f'Delta: {delta}')
+        return (round(weight[0] + delta[0], weight_rnd_int), 
+                round(weight[1] + delta[1], weight_rnd_int))
 
     @staticmethod
     def dot_product(pattern, weight):
@@ -46,11 +56,11 @@ class ADALINE:
         return total
 
     @staticmethod
-    def get_distance(pattern):
-        total = 0
+    def get_distance(pattern, pattern_rnd_int):
+        total = 0 
         for x in pattern:
-            total += x**2
-        return total
+            total += round(x**2, pattern_rnd_int)
+        return round(total, pattern_rnd_int)
 
 
 # Example from "Understanding Neural Networks, Vol 1: Basic Networks"
@@ -60,4 +70,4 @@ B = [(-0.6,0.3),(-0.4,-0.2),(0.3,-0.4),(-0.2,-0.8)]
 weight = (-0.6,0.8)       
 beta = 0.5
 
-ADALINE(A, B, weight, beta)         
+ADALINE(A, B, weight, beta, 2, 1)
